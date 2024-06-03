b55d133869131a9812ad072a3e80088cb517c21c
gionkunz <gion@syncrea.ch>
2024-06-04 00:03:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# oop-to-fp
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/gionkunz/oop-to-fp)
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+export class Developer {
+  develop() {
+    console.log('Developing...');
+  }
+}
+
+export class Designer {
+  design() {
+    console.log('Designing...');
+  }
+}
+
+/*
+ No Hybrid / Multiple Inheritance
+
+export class DeveloperDesigner extends Developer, Designer {
+
+}
+*/
