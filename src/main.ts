dca975b250e2b5fe0f792df3746c8488d7ff865b
gionkunz <gion@syncrea.ch>
2024-06-04 00:47:23 +0200

    Refactored to use some fine CB OO Mixins!

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,32 @@
-export class Developer {
-  develop() {
-    console.log('Developing...');
-  }
-}
-
-export class Designer {
-  design() {
-    console.log('Designing...');
-  }
-}
-
 /*
- No Hybrid / Multiple Inheritance
-
-export class DeveloperDesigner extends Developer, Designer {
-
-}
+  Better class-based OOP composability using some fine TS mixins :-)
 */
+
+export type Constructor = new (...args: any[]) => {};
+
+export const developerMixin = <TBase extends Constructor>(Base: TBase = class {} as TBase) => 
+  class extends Base {
+    readonly lang = 'TypeScript';
+    develop() {
+      console.log(`Developing in ${this.lang}...`);
+    }
+  };
+
+export const designerMixin = <TBase extends Constructor>(Base: TBase = class {} as TBase) =>
+  class extends Base {
+    readonly color = 'blue';
+    design() {
+      console.log(`Designing in ${this.color}...`);
+    }
+  };
+
+export class DeveloperDesigner extends designerMixin(developerMixin()) {
+  designAndDevelop() {
+    console.log(`Designing and developing something ${this.color} in ${this.lang}`);
+  }
+}
+
+const d = new DeveloperDesigner();
+d.develop();
+d.design();
+d.designAndDevelop();
